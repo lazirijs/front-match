7fcc60f7794f6f654c7daf9420bfdc68257442f5
Laziri <109730158+lazirijs@users.noreply.github.com>
2024-02-24 23:42:42 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    name: "Org Match",
+    themeColor: "#fff",
+    msTileColor: "#fff",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#fff",
+  }
 })
