edcb27f533a5675b4bff88fc8d7242c955d12539
Laziri <109730158+lazirijs@users.noreply.github.com>
2024-02-28 17:21:48 +0100

    Update input.vue

--- src/components/utilities/input.vue
+++ src/components/utilities/input.vue
@@ -3,7 +3,7 @@
         class="w-full h-12 flex items-center bg-v rounded-v gap-[0.375rem] sm:gap-2 md:gap-3 px-2 sm:px-3 md:px-4 py-2 border-2 smooth"
         :class="{'border-red-500 border-dashed': invalid, 'bg-opacity-70 cursor-default': loading || readonly }">
         <icon-app v-if="icon" :icon="loading ? 'svg-spinners:ring-resize' : icon" class="w-9" />
-        <input dir="auto" :autocomplete="autocomplete" :name="name || label" class="w-full placeholder:tracking-normal truncate" :readonly="readonly" @:input="emits('update', $event.target.value)"
+        <input dir="rtl" :autocomplete="autocomplete" :name="name || label" class="w-full placeholder:tracking-normal truncate" :readonly="readonly" @:input="emits('update', $event.target.value)"
             :value="value" :type="type" :placeholder="placeholder || 'enter text here'" :min="min" :max="max" :maxlength="maxlength" :minlength="minlength"
             :class="{ 'text-center placeholder:text-center': center, 'cursor-default': loading || readonly }" :required="required" :accessKey="accessKey" />
         <icon-app v-if="btn || (icon && center)" @click="emits('btn')" :icon="btn || center" class="w-9" />    
