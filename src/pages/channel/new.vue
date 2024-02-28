ba6961cf0fd9575df5426ad70c6d18f90caa09a1
Laziri <109730158+lazirijs@users.noreply.github.com>
2024-02-28 17:09:18 +0100

    Update new.vue

--- src/pages/channel/new.vue
+++ src/pages/channel/new.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="grid gap-20 py-6">
-    <h1 class="font-bold text-start">New Channel</h1>
+    <h1 class="font-bold text-start">قناة جديدة</h1>
     
     <form @submit.prevent="submitForm" class="grid gap-8">
-      <input-app :value="channel.name" @update="channel.name = $event" icon="ic:round-tv" placeholder="channel name" :readonly="loading" />
-      <input-app :value="channel.category" @update="channel.category = $event" icon="tabler:category-2" placeholder="channel category" :readonly="loading" />
+      <input-app :value="channel.name" @update="channel.name = $event" icon="ic:round-tv" placeholder="اسم القناة" :readonly="loading" />
+      <input-app :value="channel.category" @update="channel.category = $event" icon="tabler:category-2" placeholder="فئة القناة" :readonly="loading" />
       <div class="space-y-2 overflow-y-auto">
-        <input-app v-for="(item, index) in channel.responsible" :key="index" :value="channel.responsible[index]" @update="channel.responsible[index] = $event" icon="fluent:person-24-filled" placeholder="channel responsible" :readonly="loading" />
-        <h6 class="text-gray-400 text-start first-letter:lowercase px-2"><a @click="channel.responsible.push('')" class="underline cursor-pointer">click here</a> to add another responsible</h6>
+        <input-app v-for="(item, index) in channel.responsible" :key="index" :value="channel.responsible[index]" @update="channel.responsible[index] = $event" icon="fluent:person-24-filled" placeholder="مسؤولة القناة" :readonly="loading" />
+        <h6 class="text-gray-400 text-start first-letter:lowercase px-2"><a @click="channel.responsible.push('')" class="underline cursor-pointer">انقر هنا</a> لإضافة مسؤول آخر </h6>
       </div>
       <div class="space-y-2">
         <div class="grid grid-cols-4 gap-4">
@@ -16,7 +16,7 @@
           <div @click="loading ? '' : channel.languages.en = !channel.languages.en" class="w-full h-12 border-2 border-solid border-gray-300 text-gray-500 rounded-v flex-center smooth cursor-pointer" :class="{ 'bg-gray-200 text-gray-700' : channel.languages.en }">En</div>
           <div @click="loading ? '' : channel.languages.fr = !channel.languages.fr" class="w-full h-12 border-2 border-solid border-gray-300 text-gray-500 rounded-v flex-center smooth cursor-pointer" :class="{ 'bg-gray-200 text-gray-700' : channel.languages.fr }">Fr</div>
         </div>
-        <h6 class="text-gray-400 text-start first-letter:lowercase px-2">click to select channel languages</h6>
+        <h6 class="text-gray-400 text-start first-letter:lowercase px-2">انقر لتحديد لغات القناة</h6>
         <button @click="create()" class="hidden"/>
       </div>
     </form>
@@ -24,7 +24,7 @@
     <div class="space-y-4">
       <btn-app text="create" @click="create()" icon="entypo:plus" :loading="loading" dark class="min-w-fit mt-4 mx-auto" />
       <h6 class="w-5/12 m-auto text-gray-400 first-letter:lowercase">
-        you can't edit channel info after creating it
+        لا يمكنك تعديل معلومات القناة بعد إنشائها
       </h6>
     </div>
   </div>
@@ -68,7 +68,7 @@ const create = async () => {
   } catch (error) {
     loading.value = false;
     console.error(error);
-    alert("An error occurred during creation channel");
+    alert("حدث خطأ أثناء إنشاء القناة");
   }
 }
 </script>
