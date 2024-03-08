99cf2f2ce4314014192b1b0b590259875267d2a3
Laziri <109730158+lazirijs@users.noreply.github.com>
2024-03-08 19:15:36 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,47 @@ const payed = ref(true);
 (async () => {
   const result = await api.get('/payed');
   payed.value = result.data;
-  const loginJWT = await api.get('/loginJWT');
-  console.log(loginJWT);
-  const protectedT = await api.get('/protected');
-  console.log(protectedT);
 })()
+
+// Send a request to the server to set a cookie
+function setCookie() {
+  fetch('https://back-match.onrender.com/set-cookie', {
+    method: 'GET',
+    credentials: 'include' // Include cookies in the request
+  })
+  .then(response => {
+    console.log(response);
+    if (response.ok) {
+      console.log('Cookie set successfully');
+    } else {
+      console.error('Failed to set cookie');
+    }
+  })
+  .catch(error => {
+    console.error('Error setting cookie:', error);
+  });
+}
+
+// Get the cookie value
+function getCookie(name) {
+  const cookieArr = document.cookie.split(';');
+
+  for (let i = 0; i < cookieArr.length; i++) {
+    const cookiePair = cookieArr[i].trim().split('=');
+    if (cookiePair[0] === name) {
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+
+  return null;
+}
+
+const code = prompt();
+if ( code == 'set' ) {
+  // Example usage
+  setCookie();
+} else if  ( code == 'get' ) {
+  const jwtCookie = getCookie('jwt');
+  console.log('JWT Cookie:', jwtCookie);
+}
 </script>
